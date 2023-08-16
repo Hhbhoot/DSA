30f2139ab6b9c666cd4a1ba23156a42987685a01
Hitesh <hiteshbhut666@gmail.com>
2023-08-16 18:54:15 +0530

    AND OR operator

--- AND_OR_operator.c
+++ AND_OR_operator.c
@@ -1,29 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a=1 , b=2, c;
+    // int a=1 , b=2, c;
 
-   // c= a && b;
+    // c= a && b;
 
     // printf("a : %d , b : %d , c : %d\n",a,b,c); // 1 1 1
 
     // c= a && --b;
-    //printf("a : %d , b : %d , c : %d\n",a,b,c); // 1 1 1
+    // printf("a : %d , b : %d , c : %d\n",a,b,c); // 1 1 1
 
     // c= --a && --b;
-   // printf("a : %d , b : %d , c : %d\n",a,b,c); // 0 2 0
-
-
+    // printf("a : %d , b : %d , c : %d\n",a,b,c); // 0 2 0
 
     // c= --a && b;
-    //printf("a : %d , b : %d , c : %d",a,b,c); // 1 1 1
+    // printf("a : %d , b : %d , c : %d",a,b,c); // 1 1 1
 
     //  c= ++a && b;
 
     //  printf("a : %d , b : %d , c : %d\n",a,b,c); // 2 2 1
 
-      c=2 ;
-      int d ;
+    //   c=2 ;
+    //   int d ;
 
     //   d=a && b&& c;
 
@@ -33,32 +31,27 @@ int main()
 
     //  printf("a : %d , b : %d , c : %d , d: %d\n",a,b,c,d); // 0 2 2 0
 
-     //  d=a && ++b&& --c;
+    //  d=a && ++b&& --c;
 
-     //printf("a : %d , b : %d , c : %d , d: %d\n",a,b,c,d); // 1 3 3 1
+    // printf("a : %d , b : %d , c : %d , d: %d\n",a,b,c,d); // 1 3 3 1
 
     //     d=a && (b || c );
 
     //  printf("a : %d , b : %d , c : %d , d: %d\n",a,b,c,d); // 1 2 2 1
 
-       d=1;
+    //    d=1;
 
-       int e ;
+    //    int e ;
     //    e=a || (b && c ) || d ;;
 
     //  printf("a : %d , b : %d , c : %d , d: %d  e : %d\n",a,b,c,d,e); // 1 2 2 1 1
 
-       
-       e= --a || (b && c ) || ++d ;;
+    //    e= --a || (b && c ) || ++d ;;
 
-     printf("a : %d , b : %d , c : %d , d: %d  e : %d\n",a,b,c,d,e);
+    //  printf("a : %d , b : %d , c : %d , d: %d  e : %d\n",a,b,c,d,e);
+    int a = 1, b = 2, c = 1, d;
 
-    
+    d = ((++a || --b) && --c) || ++a;
 
-
-
-
-
-
-
+    printf("a : %d , b : %d , c %d,d :  %d\n", a, b, c, d);
 }
